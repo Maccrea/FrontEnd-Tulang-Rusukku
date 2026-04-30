9429187af87cf73365536573ece75bbb550b2cc3
Maccrea <macreynardosan@gmail.com>
2026-04-30 16:53:38 +0700

    feat: [MCR] fix index and add SplashScreen for splashscreen system

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,5 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { useRouter } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
+import { Redirect } from 'expo-router';
 
-export default function SplashScreen() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.replace('/OnBoard'); 
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <Image 
-          source={require('../../assets/images/logo.png')} 
-          style={styles.logo} 
-          resizeMode="contain"
-        />
-        <Text style={styles.title}>Tulang Rusukku</Text>
-      </View>
-
-    </View>
-  );
+export default function Index() {
+  return <Redirect href={"/SplashScreen" as any} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2A2A30',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  content: {
-    alignItems: 'center',
-  },
-  logo: {
-    width: 140,
-    height: 140,
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#A682DF',
-    letterSpacing: 0.5,
-  },
-});
